9e92bacb0f92bbdc507c43bdcfc700bc97a28723
btilton <brian@tilton.xyz>
2015-07-16 15:38:56 -0700

    autorespond.sh works now
    Uses gam.py to do all of the following:
    - Reactivates a deactivated Google Apps account
    - Sets the subject of the vacation responder.
      - If no flag, default subject line is used
      - If flag is set with no argument passed the subject is read through cat
      - If flag is set with argument passed, argument is set as subject line
    - Sets the message body of the vacation responder
      - If no flag, default message body is used
      - If flag is set with no argument passed the message body is read through cat
      - If flag is set with argument passed, argument is set as message body
    - Sets start date of vacation responder to current date
    - Sets end date of vacation responder to 50 years from current date

--- autorespond.sh
+++ autorespond.sh
@@ -1,59 +1,125 @@
 #!/bin/bash
-#################
+
+###############################
 # Author: Brian Tilton
 # Date  : 2015-06-26
 # Title : autorespond.sh
 # Desc  : Script to reactivate A Google Apps account and then
 #       :   set a 50 year away message using GAM
-#################
+################################
 
-# Set Date Variables
+##### INIT VARIABLES #####
+
+# Change this to point to your gam.py file
+gamfile="/Users/btilton/gam/gam.py"
+
+gam="python $gamfile"
+
+# Dates
 start_date=`date +%Y-%m-%d` #Start date for vacation message in correct format
 end_date=`date -v+50y +%Y-%m-%d` #End date for vacation message. 50 years from now
 
-usermail=''
-
-#print usage function
-function usage {
-  echo "Usage: autorespond.sh [-s subject] [-m message] [user_email_address]"
-  echo "       autorespond.sh [-h]"
-  exit 1
-}
+# Set username from argument passed in last position
+usermail="${@: -1}"
 
 #Default subject and message variable contents
 subject="Thank you for your message"
 message="Thank you for your email. The owner of this email address no longer works for Upwork."
 
-##### MAIN
 
-while getopts 's:m:h' flag; do
-  case ${flag} in
-    s ) subject="${OPTARG}" ;;
-    m ) message="${OPTARG}" ;;
-    h ) usage ;;
-    * ) error "Unexpected option ${flag}"
-        usage
-        ;;
-  esac
-done
+##### FUNCTIONS #####
 
-usermail=$1
-if [$usermail = '']; then
-  usage
+#print usage function
+function usage {
+    echo "Usage: autorespond.sh [-s subject] [-m message] [user_email_address]"
+    echo "       autorespond.sh [-h]"
+    exit 1
+}
+
+function inSubMsg () {
+    if [ "$1" == "Sub" ]; then
+        echo $'Enter the auto-response subject line.\nPress Enter then crtl-d when done'
+        subject=$(cat)
+    elif [ "$1" == "Msg" ]; then
+        echo $'Enter the auto-response message body.\nPress Enter then crtl-d when done'
+        message=$(cat)
+    fi
+}
+
+
+##### MAIN #####
+
+# If -h or empty print usage
+if [[ ($usermail == '') || ($usermail == '-h') || ($usermail == '--help') ]]; then
+    usage
 fi
 
 #Check if user exists
-userexists=`$gam user info $usermail | sed -n 1p | awk '{print $1;}' | cut -d: -f 1`
+echo "Checking if user $usermail exists..."
+userexists=$($gam info user $usermail | sed -n 1p | awk '{print $1;}' | cut -d: -f1)
 
-if [[ $userexists == Error ]]; then
-  echo "User does not exist in Google Apps"
-  exit 1
-fi
+# Script options handling
+while [[ $# -gt 1 ]]; do
+    case "$1" in
+        -s|--subject)
+            shift
+            echo $1
+            if [[ ("$1" != "$usermail") && ("$1" != "-m") && ("$1" != "-h") ]]; then
+                subject="$1"
+                shift
+            elif [[ "$1" == "-m" ]]; then
+                inSubMsg Sub
+                shift
+                if [[ ("$1" != "$usermail") && ("$1" != '-s') && ("$1" != '-h') ]]; then
+                    message=$(cat)
+                    shift
+                elif [[ ("$1" == '-h') || ("$1" == '-s') ]]; then
+                    usage
+                else
+                    inSubMsg Msg
+                fi
+            elif [[ "$1" == "-h" ]]; then
+                usage
+            else
+                inSubMsg Sub
+            fi
+            ;;
+        -m|--message)
+            shift
+            echo $1
+            if [[ ("$1" != "$usermail") && ("$1" != '-s') && ("$1" != '-h') ]]; then
+                message="$1"
+                shift
+            elif [ "$1" == '-s' ]; then
+                inSubMsg Msg
+                shift
+                if [[ ("$1" != "$usermail") && ("$1" != '-m') && ("$1" != '-h') ]]; then
+                    subject=$(cat)
+                    shift
+                elif [[ ("$1" == '-h') || ("$1" == '-m') ]]; then
+                    usage
+                else
+                    inSubMsg Sub
+                fi
+            elif [ "$1" == '-h' ]; then
+                usage
+            else
+                inSubMsg Msg
+            fi
+            ;;
+        -h|--help)
+            usage
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
 
 if [[ $userexists == User ]]; then
-  $gam update user $usermail suspended off &&
+    $gam update user $usermail suspended off &&
 
-  $gam user $usermail vacation on subject "$subject" message "$message" startdate $start_date enddate $end_date &&
+    $gam user $usermail vacation on subject "$subject" message "$message" startdate $start_date enddate $end_date &&
 
-  $gam user $usermail show vacation
+    $gam user $usermail show vacation
 fi
